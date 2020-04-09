9a43266ace12f4b517b7b1bf9dfb74443908f3c0
savtanet <63137602+savtanet@users.noreply.github.com>
2020-04-09 23:37:20 +0300

    mutiple fields for registration form
    SQL still not commit

--- SavtaApp/main.py
+++ SavtaApp/main.py
@@ -63,12 +63,16 @@ def logout():
 def register():
     # Output message if something goes wrong...
     msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+    # Check if "username", "password" and "email" and "address" and "birthday" and "skills" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'address' in request.form and 'birhtday' in request.form and 'skills' in request.form:
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
+        address = request.form['address']
+        birhtday = request.form['birhtday']
+        skills = request.form['skills']
+        
                 # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
@@ -80,16 +84,16 @@ def register():
             msg = 'Invalid email address!'
         elif not re.match(r'[A-Za-z0-9]+', username):
             msg = 'Username must contain only characters and numbers!'
-        elif not username or not password or not email:
+        elif not username or not password or not email or not address or not birthday or not skills:
             msg = 'Please fill out the form!'
         else:
             # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            cursor.execute('INSERT INTO accounts(id, username, password, email, address, birthday, skills) VALUES (NULL, %s, %s, %s, %s, %s, %s);')
             mysql.connection.commit()
             msg = 'You have successfully registered!'
     elif request.method == 'POST':
         # Form is empty... (no POST data)
-        msg = 'Please fill out the form!'
+        msg = 'Please fill out the form, 	no post data!'
     # Show registration form with message (if any)
     return render_template('register.html', msg=msg)
 
