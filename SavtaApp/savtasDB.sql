460df7f8b38946eacf7de1aabd21afea4f0fa3d1
savtanet <63137602+savtanet@users.noreply.github.com>
2020-04-10 19:51:08 +0300

    fixed SAVTAS SQL

--- SavtaApp/savtasDB.sql
+++ SavtaApp/savtasDB.sql
@@ -1,6 +1,10 @@
-  CREATE TABLE `savtas` (
-    `id` identity,
-    `name` varchar(255) NOT NULL,
+CREATE DATABASE IF NOT EXISTS `pythonlogin` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `pythonlogin`;
+
+CREATE TABLE IF NOT EXISTS `savtas` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `username` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
     `birtday` varchar(255) NOT NULL,
     `city` varchar(255) NOT NULL,
     `street` varchar(255) NOT NULL,
@@ -12,7 +16,7 @@
     `limitations` varchar(20) NOT NULL,
     `livingwith` varchar(50) NOT NULL,
     PRIMARY KEY (`id`)
-  )''' )
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-
- id name birtday city street house appatrment floor elevator phone limitations livingwith
+INSERT INTO `savtas` (`id`, `username`, `password`, `birtday`, `city`, `street`, `house`, `appatrment`, `floor`, `elevator`, `phone`, `limitations`, `livingwith`) 
+VALUES (1, 'test', 'test', '110370', 'ashdod', 'main', '5', '3', '2', '0', '123123123', 'limit asdasdasd', 'aba' );
