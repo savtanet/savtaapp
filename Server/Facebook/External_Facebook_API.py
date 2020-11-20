30ad277e778957a094862831c7b6488ebe854324
AnthonGar <anthon_1_2@yahoo.com>
2020-11-20 11:31:10 +0200

    save before forrmatting my pc

--- Server/Facebook/External_Facebook_API.py
+++ Server/Facebook/External_Facebook_API.py
@@ -6,7 +6,7 @@ def get_page_post_generator(url, number_of_pages=1, credentials=("mysavta.test1@
 
 
 def get_group_post_generator(group, number_of_pages=1, credentials=("mysavta.test1@gmail.com", "TempPassword123")):
-    return  get_posts(group=group, pages=number_of_pages, credentials=credentials)
+    return get_posts(group=group, pages=number_of_pages, credentials=credentials)
 
 
 # Doesn't work for users.
@@ -14,6 +14,6 @@ def get_user_post_generator(user_id, number_of_pages=1, credentials=("mysavta.te
     return get_posts(user_id, pages=number_of_pages, credentials=credentials)
 
 
-gen = get_group_post_generator("Israel.Volunteering")
+gen = get_group_post_generator("Israel.Volunteering", number_of_pages=1)
 for post in gen:
-    print(post)
+    print(post['user_id'])
